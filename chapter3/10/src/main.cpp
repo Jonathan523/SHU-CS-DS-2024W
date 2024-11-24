2c55b385eb5ac4a60583940c342edf2b057e6a38
Jonathan Zhang <admin@cestlavie.moe>
2024-11-24 18:44:16 +0800

    refactor: optimize the code structure and modify output function

--- chapter3/10/src/main.cpp
+++ chapter3/10/src/main.cpp
@@ -5,28 +5,24 @@
 int main()
 {
     LinkList list1;
-    list1.insertasc(1);
-    list1.insertasc(3);
-    list1.insertasc(5);
-    list1.insertasc(7);
 
+    list1.insertasc(7);
+    list1.insertasc(5);
+    list1.insertasc(3);
+    list1.insertasc(1);
     LinkList list2;
     list2.insertasc(2);
     list2.insertasc(4);
     list2.insertasc(6);
     list2.insertasc(8);
 
-    std::cout << "List 1: ";
-    list1.display();
+    std::cout << "List 1: " << list1;
+    std::cout << "List 2: " << list2;
 
-    std::cout << "List 2: ";
-    list2.display();
+    LinkList mergedList = LinkList::merge(list1, list2);
+    LinkList reversedList = LinkList::reverse(mergedList);
 
-    LinkList mergedList = LinkList::mergeLists(list1, list2);
-    mergedList.reverse();
-
-    std::cout << "Merged and reversed list: ";
-    mergedList.display();
+    std::cout << "Merged and reversed list: " << reversedList;
 
     return 0;
 }
