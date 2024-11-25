1e1ff1501ab6edbca3a21f2f3bfdc618b4dc9dc6
lyyyyrrrr <2842945065@qq.com>
2024-11-25 13:09:01 +0800

    3.7(new)

--- chapter3/7/src/Polynomial.cpp
+++ chapter3/7/src/Polynomial.cpp
@@ -1,14 +1,21 @@
 #include "Polynomial.hpp"
-#include <cmath> 
+#include <cmath>
 
-Node::Node(int coeff, int exp) : coefficient(coeff), exponent(exp), next(NULL) {}
-
-Polynomial::Polynomial() {
-    head = new Node();
-    head->next = head; 
+Node::Node(double coeff, double exp)
+    : coefficient(coeff), exponent(exp), next(NULL)
+{
 }
+// 内联构造函数
 
-Polynomial::~Polynomial() {
+Polynomial::Polynomial()
+{
+    head = new Node();
+    head->next = head;
+}
+// 创建头结点
+
+Polynomial::~Polynomial()
+{
     Node* current = head->next;
     while (current != head) {
         Node* temp = current;
@@ -18,35 +25,48 @@ Polynomial::~Polynomial() {
     delete head;
 }
 
-void Polynomial::addTerm(int coefficient, int exponent) {
+void Polynomial::add(double coefficient, double exponent)
+{
     Node* newNode = new Node(coefficient, exponent);
     Node* current = head;
     while (current->next != head && current->next->exponent > exponent) {
         current = current->next;
     }
-    newNode->next = current->next;
-    current->next = newNode;
+    if (current->exponent == newNode->exponent) {
+        current->coefficient += newNode->coefficient;
+    } else {
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+}
+// 根据指数大小插入
+
+void Polynomial::show(int num)
+{
+    Node* current = head->next;
+    if (current->coefficient > 0)
+        cout << current->coefficient << "x^" << current->exponent;
+    else
+        cout << current->coefficient << "x^" << current->exponent;
+    current = current->next;
+    for (int i = 0; i < num - 1; i++) {
+        if (current->coefficient > 0)
+            cout << "+" << current->coefficient << "x^" << current->exponent;
+        else
+            cout << "-" << abs(current->coefficient) << "x^"
+                 << current->exponent;
+        current = current->next;
+    }
 }
 
-double Polynomial::calc(double x) const {
-    if (x == 0) {
-        double result = 0.0;
-        Node* current = head->next;
-        while (current != head) {
-            if (current->exponent == 0) {
-                result += current->coefficient;
-            }
-            current = current->next;
-        }
-        return result;
+double Polynomial::calc(double x) const
+{
+
+    double result = 0.0;
+    Node* current = head->next;
+    while (current != head) {
+        result += current->coefficient * pow(x, current->exponent);
+        current = current->next;
     }
-    else {
-        double result = 0.0;
-        Node* current = head->next;
-        while (current != head) {
-            result += current->coefficient * pow(x, current->exponent);
-            current = current->next;
-        }
-        return result;
-    }
+    return result;
 }
--- chapter3/7/src/main.cpp
+++ chapter3/7/src/main.cpp
@@ -1,19 +1,36 @@
-#include <iostream>
 #include "Polynomial.hpp"
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     Polynomial poly;
-    poly.addTerm(6, 3);
-    poly.addTerm(5, 2); 
-    poly.addTerm(4, 1); 
-    poly.addTerm(3, 0); 
-    cout << "6x^3+5x^2+4x+3="<<endl;
-
+    int num = 0, flag = 0;
+    cout << "请输入多项式（系数，指数）——(如系数输入为0则退出)：";
+    while (true) {
+        double m = 0, n = 0;
+        cin >> m;
+        if (m == 0)
+            break;
+        cin >> n;
+        if (0 < n && n < 1)
+            flag = 1;
+        else if (n <= -1)
+            flag = 2;
+        else if (-1 < n && n < 0)
+            flag = 3;
+        poly.add(m, n);
+        num++;
+    }
+    poly.show(num);
+    cout << endl;
     double x = 0.0;
     cout << "please input the 'x':" << endl;
     cin >> x;
-    cout << "The value of the polynomial at x = " << x << " is " << poly.calc(x) << endl;
-
+    if ((flag == 1 && x < 0) || (flag == 2 && x == 0) || (flag == 3 && x <= 0))
+        cout << "Invalid input value" << endl;
+    else
+        cout << "The value of the polynomial at x = " << x << " is "
+             << poly.calc(x) << endl;
     return 0;
 }
