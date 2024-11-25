ae7f9c236ec80a49bab621e34cfb5e0deb8ccd04
ZHANGss-ss <1902525897@qq.com>
2024-11-25 14:10:06 +0800

    fix: Add define

--- chapter3/6/src/main.cpp
+++ chapter3/6/src/main.cpp
@@ -4,31 +4,33 @@ using namespace std;
 
 int main()
 {
-      int e,n;
+    int e, n;
     LinkList Link;
 
-
-    cout<<"输入元素数量：";
-    cin>>n;
+#ifdef DS_DEBUG
+    cout << "输入元素数量：";
+#endif
+    cin >> n;
     int k;
-    for(int i=0;i<n;i++)
-    {
-        cin>>k;
+    for (int i = 0; i < n; i++) {
+        cin >> k;
         Link.append(k);
     }
 
     cout << "LinkList: " << endl;
     Link.ShowLinkList();
     cout << '\n';
-
+#ifdef DS_DEBUG
     cout << "Please input the element you want to insert : ";
+#endif
     cin >> e;
     Link.Insert(e);
     cout << "LinkList: " << endl;
     Link.ShowLinkList();
     cout << '\n';
-
+#ifdef DS_DEBUG
     cout << "Please input the element you want to delete : ";
+#endif
     cin >> e;
     Link.Delete(e);
 }
