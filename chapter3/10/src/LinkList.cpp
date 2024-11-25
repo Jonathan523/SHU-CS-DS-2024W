c447a02b173fb8476510b7ddd03895c987d29128
Jonathan Zhang <admin@cestlavie.moe>
2024-11-25 14:03:34 +0800

    fix: Merge function merge and reverse (#13)

--- chapter3/10/src/LinkList.cpp
+++ chapter3/10/src/LinkList.cpp
@@ -1,5 +1,6 @@
 // LinkList.cpp
 #include "LinkList.hpp"
+#include <cstddef>
 #include <iostream>
 
 LinkList::LinkList() : head(NULL) {}
@@ -36,44 +37,40 @@ void LinkList::insert(int val)
     head = newNode;
 }
 
-LinkList LinkList::merge(const LinkList& list1, const LinkList& list2)
+LinkList LinkList::merge_and_reverse(const LinkList& list1, const LinkList& list2)
 {
-    LinkList mergedList;
+    LinkList mergedAndReversedList;
     Node* current1 = list1.head;
     Node* current2 = list2.head;
 
-    while ((current1 != NULL) && (current2 != NULL)) {
-        if (current1->data <= current2->data) {
-            mergedList.insertasc(current1->data);
-            current1 = current1->next;
+    while (current1 != NULL || current2 != NULL) {
+        if (current1 != NULL && current2 != NULL) {
+            if (current1->data == current2->data) {
+                mergedAndReversedList.insert(current1->data);
+                mergedAndReversedList.insert(current2->data);
+                current1 = current1->next;
+                current2 = current2->next;
+            } else if (current1->data < current2->data) {
+                mergedAndReversedList.insert(current1->data);
+                current1 = current1->next;
+            } else {
+                mergedAndReversedList.insert(current2->data);
+                current2 = current2->next;
+            }
+        } else if (current1 == NULL) {
+            while (current2 != NULL) {
+                mergedAndReversedList.insert(current2->data);
+                current2 = current2->next;
+            }
         } else {
-            mergedList.insertasc(current2->data);
-            current2 = current2->next;
+            while (current1 != NULL) {
+                mergedAndReversedList.insert(current1->data);
+                current1 = current1->next;
+            }
         }
     }
 
-    while (current1 != NULL) {
-        mergedList.insertasc(current1->data);
-        current1 = current1->next;
-    }
-
-    while (current2 != NULL) {
-        mergedList.insertasc(current2->data);
-        current2 = current2->next;
-    }
-
-    return mergedList;
-}
-
-LinkList LinkList::reverse(const LinkList& list)
-{
-    LinkList reversedList;
-    Node* current = list.head;
-    while (current != NULL) {
-        reversedList.insert(current->data);
-        current = current->next;
-    }
-    return reversedList;
+    return mergedAndReversedList;
 }
 
 std::ostream& operator<<(std::ostream& os, const LinkList& list)
