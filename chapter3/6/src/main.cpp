71172d4c03cb40970ddc1825ff723b14f358c204
ZHANGss <1902525897@qq.com>
2024-11-25 21:25:48 +0800

    fix:  change to variable LinkList, change the method of Insert and Delete (#14)
    * fix:  change to variable LinkList, change the method of Insert and Delete
    
    * fix: Add input data
    
    * fix: Add define

--- chapter3/6/src/main.cpp
+++ chapter3/6/src/main.cpp
@@ -4,27 +4,33 @@ using namespace std;
 
 int main()
 {
-    int e;
+    int e, n;
     LinkList Link;
-    Link.append(1);
-    Link.append(3);
-    Link.append(6);
-    Link.append(7);
-    Link.append(8);
-    Link.append(11);
+
+#ifdef DS_DEBUG
+    cout << "输入元素数量：";
+#endif
+    cin >> n;
+    int k;
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        Link.append(k);
+    }
 
     cout << "LinkList: " << endl;
     Link.ShowLinkList();
     cout << '\n';
-
+#ifdef DS_DEBUG
     cout << "Please input the element you want to insert : ";
+#endif
     cin >> e;
     Link.Insert(e);
     cout << "LinkList: " << endl;
     Link.ShowLinkList();
     cout << '\n';
-
+#ifdef DS_DEBUG
     cout << "Please input the element you want to delete : ";
+#endif
     cin >> e;
     Link.Delete(e);
 }
