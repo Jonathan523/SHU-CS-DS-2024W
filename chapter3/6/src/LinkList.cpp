557f38423aa64d3268177066497bc8f22043fea2
ZHANGss-2005 <1902525897@qq.com>
2024-11-25 12:05:08 +0800

    fix: change to variable LinkList ,change the method of Insert and Delete

--- chapter3/6/src/LinkList.cpp
+++ chapter3/6/src/LinkList.cpp
@@ -27,27 +27,28 @@ void LinkList::Insert(int e)  // 将元素e插入有序链表中
 {
     Node* p = new Node(e);
     Node* q = head;
+    
     while (p->data > q->next->data) {
         q = q->next;
         if (q->next == NULL)
             break;
     }
-    p->next=q->next;
-    q->next=p;
+    p->next = q->next;
+    q->next = p;
 }
 
 void LinkList::Delete(int e)  // 删除数据元素等于e的所有结点
 {
     bool flag = false;
     Node* p = head;
-    while (p != NULL && p->next != NULL) {
-        if (e == p->next->data) {
-            p->next = p->next->next;
-            flag = true;
-        } else {
-            p = p->next;
-        }
+    while (p->next != NULL && p->next->data < e)
+        p = p->next;
+    while (p->next != NULL && e == p->next->data) {
+        p->next = p->next->next;
+        flag = true;
+        if(p->next->data>e) break;
     }
+
     if (flag == false) {
         cout << "!!!!!!!!!!There has no " << e << endl;
         cout << "LinkList: " << endl;
