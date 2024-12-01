efd48d461e0d94e7c1690a7ccb8e3fd520cd918f
Jonathan Zhang <admin@cestlavie.moe>
2024-12-01 23:22:30 +0800

    feat: 4.7

---
+++ chapter4/7/include/Queue.hpp
@@ -0,0 +1,20 @@
+#pragma once
+class Queue
+{
+private:
+    int* data;         // 队列存储空间
+    int maxSize;    // 队列最大容量
+    int frontIndex;  // 队头索引
+    int rearIndex;   // 队尾索引
+    int size;        // 队列当前大小
+
+public:
+    Queue(int maxSize);
+    ~Queue();
+    bool isEmpty() const;
+    bool isFull() const;
+    void push(int value);
+    void pop();
+    int front() const;
+    void print() const;
+};
---
+++ chapter4/7/src/Queue.cpp
@@ -0,0 +1,74 @@
+#include "Queue.hpp"
+#include <iostream>
+
+
+Queue::Queue(int maxSize) : maxSize(maxSize), frontIndex(0), rearIndex(0), size(0)
+{
+    data = new int[maxSize];
+}
+
+
+Queue::~Queue()
+{
+    delete[] data;
+}
+
+
+bool Queue::isEmpty() const
+{
+    return size == 0;
+}
+
+
+bool Queue::isFull() const
+{
+    return size == maxSize;
+}
+
+
+void Queue::push(int value)
+{
+    if (isFull()) {
+        std::cerr << "队列已满，无法插入元素！" << '\n';
+        return;
+    }
+    data[rearIndex] = value;
+    rearIndex = (rearIndex + 1) % maxSize;
+    ++size;
+}
+
+
+void Queue::pop()
+{
+    if (isEmpty()) {
+        std::cerr << "队列为空，无法删除元素！" << '\n';
+        return;
+    }
+    frontIndex = (frontIndex + 1) % maxSize;
+    --size;
+}
+
+
+int Queue::front() const
+{
+    if (isEmpty()) {
+        std::cerr << "队列为空，无队头元素！" << '\n';
+        return int();
+    }
+    return data[frontIndex];
+}
+
+void Queue::print() const
+{
+    std::cout<<"\nQueue: ";
+    if (isEmpty()) {
+        std::cerr << "队列为空！" << '\n';
+        return;
+    }
+    int index = frontIndex;
+    for (int i = 0; i < size; ++i) {
+        std::cout << data[index] << " ";
+        index = (index + 1) % maxSize;
+    }
+    std::cout << '\n';
+}
