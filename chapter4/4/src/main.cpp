44fcc2c3db75d646ecbfff4f82f2c9ebffaa9bce
Jonathan Zhang <admin@cestlavie.moe>
2024-12-02 00:02:15 +0800

    refactor: format code & optimize output

--- chapter4/4/src/main.cpp
+++ chapter4/4/src/main.cpp
@@ -1,54 +1,75 @@
-#include <iostream>
 #include "stack.hpp"
+#include <iostream>
 
-int main() {
+int main()
+{
     int maxsize;
+#ifdef DS_DEBUG
     std::cout << "Please enter the maximum size of the stack: ";
+#endif
     std::cin >> maxsize;
     Stack sqe_stack(maxsize);
-
-    std::cout << "Please input the value of stack1 (end with Enter):\n";
-    std::cin.ignore(); // ����֮ǰ����Ļ��з�
+#ifdef DS_DEBUG
+    std::cout << "Please input the value of stack1 (end with Enter): ";
+#endif
+    std::cin.ignore();
     char c;
-    while (std::cin.get(c) && c != '\n') {
-        if (isdigit(c)) {
+    while ((std::cin.get(c) != 0) && c != '\n') {
+        if (isdigit(c) != 0) {
             sqe_stack.push1(c - '0');
         }
     }
-
-    std::cout << "Please input the value of stack2 (end with Enter):\n";
-    while (std::cin.get(c) && c != '\n') {
-        if (isdigit(c)) {
-            sqe_stack.push2(c - '0'); 
+#ifdef DS_DEBUG
+    std::cout << "Please input the value of stack2 (end with Enter): ";
+#endif
+    while ((std::cin.get(c) != 0) && c != '\n') {
+        if (isdigit(c) != 0) {
+            sqe_stack.push2(c - '0');
         }
     }
 
     char choose;
+#ifdef DS_DEBUG
     std::cout << "If you want to pop the value from stack1 - y/n: ";
+#endif
     std::cin >> choose;
     if (choose == 'y' || choose == 'Y') {
         int k;
+#ifdef DS_DEBUG
         std::cout << "How much? ";
+#endif
         std::cin >> k;
-        while(k--)
-        sqe_stack.pop1();
+        while ((k--) != 0) {
+            sqe_stack.pop1();
+        }
+        std::cout << '\n';
     }
 
+#ifdef DS_DEBUG
     std::cout << "If you want to pop the value from stack2 - y/n: ";
+#endif
     std::cin >> choose;
     if (choose == 'y' || choose == 'Y') {
         int k;
+#ifdef DS_DEBUG
         std::cout << "How much? ";
+#endif
         std::cin >> k;
-        while (k--)
+        while ((k--) != 0) {
             sqe_stack.pop2();
+        }
+        std::cout << '\n';
     }
 
+#ifdef DS_DEBUG
     std::cout << "If you want to check if the stack is empty - y/n: ";
+#endif
     std::cin >> choose;
     if (choose == 'y' || choose == 'Y') {
-        std::cout << "Stack1 is empty: " << std::boolalpha << sqe_stack.isEmpty1() << std::endl;
-        std::cout << "Stack2 is empty: " << std::boolalpha << sqe_stack.isEmpty2() << std::endl;
+        std::cout << "Stack1 is "
+                  << (sqe_stack.isEmpty1() ? "EMPTY" : "NOT EMPTY") << '\n';
+        std::cout << "Stack2 is "
+                  << (sqe_stack.isEmpty2() ? "EMPTY" : "NOT EMPTY") << '\n';
     }
 
     return 0;
