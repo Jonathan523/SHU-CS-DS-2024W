d93bd1ea9eae17e0c781fcd785ec2be952d1230d
lyyyyrrrrr <2842945065@qq.com>
2024-12-02 11:06:48 +0800

    feat: 4.4 (#19)
    * 4.4
    
    * 4.4
    
    * refactor: format code & optimize output
    
    * 4.4
    
    * refactor: format code
    
    ---------
    
    Co-authored-by: Jonathan Zhang <admin@cestlavie.moe>

---
+++ chapter4/4/include/stack.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+
+class Stack
+{
+    int top1, top2;
+    int maxsize;
+    int* array;
+
+public:
+    Stack(int max) : maxsize(max), top1(-1), top2(max) { array = new int[max]; }
+    ~Stack() { delete[] array; }
+    void push1(int value)
+    {
+        if (top1 < top2 + 1) {
+            array[++top1] = value;
+        } else {
+            std::cout << "Stack Overflow" << '\n';
+            exit(1);
+        }
+    }
+    void push2(int value)
+    {
+        if (top2 > top1 + 1) {
+            array[--top2] = value;
+        } else {
+            std::cout << "Stack Overflow" << '\n';
+            exit(1);
+        }
+    }
+    void pop1()
+    {
+        if (top1 >= 0) {
+            std::cout << array[top1--] << ' ';
+        } else {
+            std::cout << "Stack Underflow" << '\n';
+            exit(1);
+        }
+    }
+    void pop2()
+    {
+        if (top2 < maxsize) {
+            std::cout << array[top2++] << ' ';
+        } else {
+            std::cout << "Stack Underflow" << '\n';
+            exit(1);
+        }
+    }
+    bool isEmpty1() const { return top1 == -1; }
+    bool isEmpty2() const { return top2 == maxsize; }
+};
---
+++ chapter4/4/src/main.cpp
@@ -0,0 +1,88 @@
+#include "stack.hpp"
+#include <iostream>
+
+int main()
+{
+    int maxsize;
+#ifdef DS_DEBUG
+    std::cout << "Please enter the maximum size of the stack: ";
+#endif
+    std::cin >> maxsize;
+    while (maxsize <= 0) {
+        std::cout << "invalid input" << std::endl;
+        std::cin >> maxsize;
+    }
+    Stack sqe_stack(maxsize);
+#ifdef DS_DEBUG
+    std::cout << "Please input the value of stack1 (end with Enter): ";
+#endif
+    std::cin.ignore();
+    char c;
+    while ((std::cin.get(c) != 0) && c != '\n') {
+        if (isdigit(c) != 0) {
+            sqe_stack.push1(c - '0');
+        }
+    }
+#ifdef DS_DEBUG
+    std::cout << "Please input the value of stack2 (end with Enter): ";
+#endif
+    while ((std::cin.get(c) != 0) && c != '\n') {
+        if (isdigit(c) != 0) {
+            sqe_stack.push2(c - '0');
+        }
+    }
+
+    char choose;
+#ifdef DS_DEBUG
+    std::cout << "If you want to pop the value from stack1 - y/n: ";
+#endif
+    std::cin >> choose;
+    if (choose == 'y' || choose == 'Y') {
+        int k;
+#ifdef DS_DEBUG
+        std::cout << "How much? ";
+#endif
+        std::cin >> k;
+        if (k >= 0) {
+            while ((k--) != 0) {
+                sqe_stack.pop1();
+            }
+        } else {
+            std::cout << "invalid input" << std::endl;
+        }
+        std::cout << '\n';
+    }
+
+#ifdef DS_DEBUG
+    std::cout << "If you want to pop the value from stack2 - y/n: ";
+#endif
+    std::cin >> choose;
+    if (choose == 'y' || choose == 'Y') {
+        int k;
+#ifdef DS_DEBUG
+        std::cout << "How much? ";
+#endif
+        std::cin >> k;
+        if (k >= 0) {
+            while ((k--) != 0) {
+                sqe_stack.pop2();
+            }
+        } else {
+            std::cout << "invalid input" << std::endl;
+        }
+        std::cout << '\n';
+    }
+
+#ifdef DS_DEBUG
+    std::cout << "If you want to check if the stack is empty - y/n: ";
+#endif
+    std::cin >> choose;
+    if (choose == 'y' || choose == 'Y') {
+        std::cout << "Stack1 is "
+                  << (sqe_stack.isEmpty1() ? "EMPTY" : "NOT EMPTY") << '\n';
+        std::cout << "Stack2 is "
+                  << (sqe_stack.isEmpty2() ? "EMPTY" : "NOT EMPTY") << '\n';
+    }
+
+    return 0;
+}
