119e7d6eaefb5bc0d1a57be82a650ef822e33f85
ZHANGss-ss <1902525897@qq.com>
2024-12-30 23:21:29 +0800

    fix: nullptr in file

--- chapter6/14/src/Tree.cpp
+++ chapter6/14/src/Tree.cpp
@@ -8,11 +8,11 @@ char readInput()
 
 TreeNode* buildTree()
 {
-    std::cout << "请输入二叉树的根结点字符元素（空结点请输入#）：\n";
+    std::cout << "请输入二叉树的根结点字符元素（空结点请输入#):\n";
 
     char rootVal = readInput();
     if (rootVal == '#') {
-        return nullptr;
+        return NULL;
     }
 
     TreeNode* root = new TreeNode(rootVal);
@@ -23,14 +23,14 @@ TreeNode* buildTree()
         TreeNode* current = q.front();
         q.pop();
 
-        std::cout << "请输入 " << current->data << " 的左子结点（空结点请输入#）：";
+        std::cout << "请输入 " << current->data << " 的左子结点（空结点请输入#):";
         int leftVal = readInput();
         if (leftVal != '#') {
             current->lchild = new TreeNode(leftVal);
             q.push(current->lchild);
         }
 
-        std::cout << "请输入 " << current->data << " 的右子结点（空结点请输入#）：";
+        std::cout << "请输入 " << current->data << " 的右子结点（空结点请输入):";
         int rightVal = readInput();
         if (rightVal != '#') {
             current->rchild = new TreeNode(rightVal);
